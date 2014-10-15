db909e298a9070b5322a9524dc9df161025839c4
Oliver <oliver.martin.sanz@gmail.com>
2014-10-15 17:54:44 +0200

    initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sapiens\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SapiensBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ SapiensBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sapiens\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SapiensBlogBundle extends Bundle
+{
+}
